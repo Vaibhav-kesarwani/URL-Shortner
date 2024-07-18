d6bf18f3e5613a77866e55907a3f2c88409589c0
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-07-18 08:22:44 +0530

    testing the button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <nav>
-      <Link>
+      <Link to="/">
         <img src="/logo.png" className="h-16" alt="Trimer-logo" />
       </Link>
+
+      <div>
+        <Button onClick={() => navigate("/auth")}>Login</Button>
+      </div>
     </nav>
   );
 };
