a162e54fce118857ebae8a8e276f4e32667a936f
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-07-18 08:38:15 +0530

    header part is completed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,6 +9,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LinkIcon, LogOutIcon } from "lucide-react";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -25,19 +26,23 @@ const Header = () => {
           <Button onClick={() => navigate("/auth")}>Login</Button>
         ) : (
           <DropdownMenu>
-            <DropdownMenuTrigger>
+            <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
               <Avatar>
                 <AvatarImage src="https://github.com/shadcn.png" />
                 <AvatarFallback>VK</AvatarFallback>
               </Avatar>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuLabel>Vaibav Kesarwani</DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuItem>Subscription</DropdownMenuItem>
+              <DropdownMenuItem>
+                <LinkIcon className="mr-2 h-4 w-4" />
+                My Links
+              </DropdownMenuItem>
+              <DropdownMenuItem className="text-red-400">
+                <LogOutIcon className="mr-2 h-4 w-4" />
+                <span>Logout</span>
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         )}
