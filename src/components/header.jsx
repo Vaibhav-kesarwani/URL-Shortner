1286c4f6205e3498c83bc87bf2760cd8a2771339
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-07-18 08:30:33 +0530

    added the dropdown menu

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,17 @@
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Header = () => {
   const navigate = useNavigate();
+  const user = true;
 
   return (
     <nav className="py-4 flex justify-between items-center">
@@ -11,7 +20,21 @@ const Header = () => {
       </Link>
 
       <div>
-        <Button onClick={() => navigate("/auth")}>Login</Button>
+        {!user ? (
+          <Button onClick={() => navigate("/auth")}>Login</Button>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
       </div>
     </nav>
   );
